c4232403b76909186114877294c4dc71c0f9bca4
baadillahnabil <baadillahnabil@gmail.com>
2024-08-31 19:18:08 +0700

    changes on the native apps code

--- android/app/src/main/java/com/contactslite/MainActivity.kt
+++ android/app/src/main/java/com/contactslite/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.contactslite
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -18,5 +23,5 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+          DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
