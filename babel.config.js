e33f15236ffb1fb643f2963285fd034d196de577
baadillahnabil <baadillahnabil@gmail.com>
2024-08-31 19:17:00 +0700

    setup absolute path imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+          '.ios.js',
+          '.ios.ts',
+          '.ios.jsx',
+          '.ios.tsx',
+          '.android.js',
+          '.android.ts',
+          '.android.jsx',
+          '.android.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@redux': './src/redux',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'jest-hoist',
+  ],
 };
