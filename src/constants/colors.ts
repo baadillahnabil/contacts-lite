a7f85393471a2fd88580e488342fb3edb6524e70
baadillahnabil <baadillahnabil@gmail.com>
2024-08-31 19:18:18 +0700

    add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  //UI Neutrals
+  ui_grey_05: '#F2F2F2',
+  ui_grey_10: '#E4E4E3',
+  ui_grey_20: '#CBCACA',
+  ui_grey_50: '#888888',
+  ui_grey_70: '#4D4C4C',
+  ui_grey_80: '#333333',
+  ui_grey_90: '#161616',
+
+  // background
+  ui_light_selected_bg: '#E7F5FF',
+};
