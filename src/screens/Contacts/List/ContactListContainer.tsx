a680f4295f239c63618b6ca6a89d7b2946e77176
baadillahnabil <baadillahnabil@gmail.com>
2024-09-01 17:53:19 +0700

    implement selector and container of contact list

--- src/screens/Contacts/List/ContactListContainer.tsx
+++ src/screens/Contacts/List/ContactListContainer.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { PermissionsAndroid, Platform } from 'react-native';
+import Contacts from 'react-native-contacts';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
+import { ACTIONS } from '@redux/actions/contacts';
+
+import { selectContactsData } from './ContactListSelector';
 import ContactListView from './ContactListView';
 
-export type ContainerProps = {};
+const ContactListContainer = () => {
+  const dispatch = useDispatch();
+  const { contacts } = useSelector(selectContactsData, shallowEqual);
 
-const ContactListContainer = ({}: ContainerProps) => {
-  return <ContactListView />;
+  const getContacts = useCallback(async () => {
+    try {
+      const response = await Contacts.getAll();
+      dispatch(ACTIONS.contactsSaveAll(response));
+    } catch (e) {
+      console.log(e);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+        title: 'Contacts',
+        message: 'ContactsLite would like to view your contacts.',
+        buttonPositive: 'Accept',
+      })
+        .then(granted => {
+          if (granted) {
+            getContacts();
+          }
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    } else {
+      getContacts();
+    }
+  }, [dispatch, getContacts]);
+
+  return <ContactListView contacts={contacts} />;
 };
 
 export default ContactListContainer;
---
+++ src/screens/Contacts/List/ContactListSelector.ts
@@ -0,0 +1,33 @@
+import { createSelector } from 'reselect';
+
+import { AppState } from '@redux/stores/configureStore';
+import type { Contact as ContactStoreInterface } from '@redux/interfaces/contacts';
+
+export const selectContactsData = createSelector(
+  [({ ContactsReducer }: AppState) => ContactsReducer],
+  ContactsReducer => {
+    const sectionedData = ContactsReducer.data.reduce<
+      Record<string, ContactStoreInterface['data']>
+    >((acc, contact) => {
+      const { givenName } = contact;
+      const [firstLetter] = givenName;
+
+      return Object.assign(acc, {
+        [firstLetter]: [...(acc[firstLetter] || []), contact],
+      });
+    }, {});
+
+    const formattedData = Object.entries(sectionedData)
+      .sort(([titleA], [titleB]) => titleA.localeCompare(titleB))
+      .map(([title, data]) => ({
+        title,
+        data: data.sort((a, b) => a.givenName.localeCompare(b.givenName)),
+      }));
+
+    return {
+      contacts: formattedData,
+    };
+  },
+);
+
+export type PropsFromSelector = ReturnType<typeof selectContactsData>;
