55ab905657bd9c438f6d63452c0f6edc83b6aba9
baadillahnabil <baadillahnabil@gmail.com>
2024-09-01 17:53:37 +0700

    apply ui view along with styling based on the data

--- src/screens/Contacts/List/ContactListView.tsx
+++ src/screens/Contacts/List/ContactListView.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  SectionList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
+import { type PropsFromSelector } from './ContactListSelector';
 import styles from './styles';
 
-export type ViewProps = {};
+export type ViewProps = PropsFromSelector;
 
-const ContactListView = ({}: ViewProps) => {
+const ContactListView = ({ contacts }: ViewProps) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Contacts</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Contacts</Text>
+      </View>
+      <SectionList
+        sections={contacts}
+        keyExtractor={(item, index) => `${item.givenName}_${index}`}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}>
+              <View style={styles.card}>
+                {item.hasThumbnail ? (
+                  <Image
+                    resizeMode="cover"
+                    source={{ uri: item.thumbnailPath }}
+                    style={styles.cardImage}
+                  />
+                ) : (
+                  <View style={[styles.cardImage, styles.cardAvatar]}>
+                    <Text style={styles.cardAvatarText}>
+                      {item.givenName[0]}
+                    </Text>
+                  </View>
+                )}
+
+                <View style={styles.cardBody}>
+                  <Text style={styles.cardTitle}>
+                    {item.givenName} {item.familyName}
+                  </Text>
+
+                  <Text style={styles.cardPhone}>
+                    {item.phoneNumbers.length > 0
+                      ? item.phoneNumbers[0].number
+                      : '-'}
+                  </Text>
+                </View>
+
+                <View style={styles.cardAction}>
+                  <Icon color="#9ca3af" name="chevron-right" size={22} />
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.sectionTitle}>{title}</Text>
+        )}
+        style={styles.listContainer}
+      />
     </SafeAreaView>
   );
 };
--- src/screens/Contacts/List/styles.ts
+++ src/screens/Contacts/List/styles.ts
@@ -1,9 +1,78 @@
 import { StyleSheet } from 'react-native';
 
+import { Colors } from '@constants';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+  },
+
+  listContainer: {
+    paddingLeft: 16,
+  },
+
+  titleContainer: {
+    paddingVertical: 24,
+    paddingLeft: 16,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
+  },
+
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#000',
+    backgroundColor: Colors.ui_grey_05,
+    paddingVertical: 12,
+  },
+
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderColor: '#d6d6d6',
+  },
+  card: {
+    paddingVertical: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  cardImage: {
+    width: 42,
+    height: 42,
+    borderRadius: 12,
+  },
+  cardAvatar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#9ca1ac',
+  },
+  cardAvatarText: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  cardBody: {
+    marginRight: 'auto',
+    marginLeft: 12,
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#000',
+  },
+  cardPhone: {
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: '500',
+    color: '#616d79',
+    marginTop: 3,
+  },
+  cardAction: {
+    paddingRight: 16,
   },
 });
 
