cc4a34dccc4b7200f59f43ad0fc9efc51d04245c
baadillahnabil <baadillahnabil@gmail.com>
2024-09-08 15:22:29 +0700

    add some test dependencies and settings

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  modulePathIgnorePatterns: ['__mocks__'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-redux|@react-native|react-navigation|react-native-vector-icons)/)',
+  ],
 };
